1202da650deb5aa76484ace61597d7a70b56c447
Devotional Phoenix 97 <158271509+devotional-phoenix-97@users.noreply.github.com>
2025-04-08 16:28:50 +0200

    Adding auto volume remapping

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -129,6 +129,7 @@ var ErrNextLoop = utils.ErrNextLoop
 // +kubebuilder:rbac:groups="",resources=events,verbs=create
 // +kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch
 // +kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;create;watch;delete;patch
+// +kubebuilder:rbac:groups="",resources=persistentvolumes,verbs=get;list;patch;watch
 // +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;delete;patch;create;watch
 // +kubebuilder:rbac:groups="",resources=pods/status,verbs=get
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=create;list;get;watch;delete
@@ -503,6 +504,11 @@ func (r *ClusterReconciler) reconcile(ctx context.Context, cluster *apiv1.Cluste
 	}
 	if result != nil {
 		return *result, nil
+	}
+
+	err = r.reconcileRemapping(ctx, cluster, resources, instancesStatus)
+	if err != nil {
+		return ctrl.Result{}, err
 	}
 
 	// Updates all the objects managed by the controller
